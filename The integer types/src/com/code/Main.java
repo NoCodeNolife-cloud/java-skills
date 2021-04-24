c8364787a48c945752450e74983196ce3437f75e
蔡伟 <1607575130@qq.com>
2021-04-24 16:07:04 +0800

    update

---
+++ The integer types/src/com/code/Main.java
@@ -0,0 +1,19 @@
+package com.code;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("默认整数常量     =  "+16);
+        byte a=16;
+        short b=16;
+        int c=16;
+        long d=16L;
+        long e=16l;
+
+        System.out.println("byte整数       =  "+a);
+        System.out.println("short整数      =  "+b);
+        System.out.println("int整数        =  "+c);
+        System.out.println("long整数       =  "+d);
+        System.out.println("long整数       =  "+e);
+    }
+}
---
+++ final/src/com/code/Main.java
@@ -0,0 +1,15 @@
+package com.code;
+
+public class Main {
+    // static const
+    public static final double PI = 3.14;
+    // member const
+    final int y = 10;
+
+    public static void main(String[] args) {
+        // local const
+        final double x = 3.3;
+        System.out.println(x);
+        System.out.println(PI);
+    }
+}
