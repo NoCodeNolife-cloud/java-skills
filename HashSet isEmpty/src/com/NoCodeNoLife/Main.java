272ef99ef3b5ecd74ff8ee794c8217ca45e13ebd
落奈 <54143278+NoCodeNolife-cloud@users.noreply.github.com>
2021-11-05 10:12:45 +0800

    update

---
+++ HashSet isEmpty/src/com/NoCodeNoLife/Main.java
@@ -0,0 +1,33 @@
+package com.NoCodeNoLife;
+
+import java.util.HashSet;
+import java.util.Iterator;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        HashSet<Integer> hashSet = new HashSet<>();
+        hashSet.add(9);
+        hashSet.add(5);
+        hashSet.add(1);
+        hashSet.add(7);
+        hashSet.add(3);
+        hashSet.add(4);
+        hashSet.add(6);
+        hashSet.add(8);
+        hashSet.add(2);
+        hashSet.add(0);
+
+        for (Iterator<Integer> iterator = hashSet.iterator(); iterator.hasNext(); ) {
+            System.out.print(iterator.next() + " ");
+        }
+        System.out.println();
+
+        System.out.println("is the hashSet empty? " + hashSet.isEmpty());
+
+        System.out.println("clear");
+        hashSet.clear();
+        System.out.println("is the hashSet empty? " + hashSet.isEmpty());
+    }
+}
